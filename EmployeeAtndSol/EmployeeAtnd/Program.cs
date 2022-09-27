f93ef2edff4f35998cd9b31843e9eb40db3c480d
vishal0893 <113405560+vishal0893@users.noreply.github.com>
2022-09-27 16:15:39 +0530

    Add files via upload

---
+++ EmployeeAtndSol/EmployeeAtnd/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace Empp;
+class Em
+{
+    static void Main(string[] args)
+    {
+        int FULLTIME = 1; 
+        Random random =new Random();
+        int Empchk = random.Next(0, 2);
+        if(Empchk == FULLTIME)
+        {
+            Console.Write("EMPLOYEE IS PRESENT");
+        }
+        else
+        {
+            Console.Write("EMPLOYEE IS ABSENT");
+        }
+    }
+}
